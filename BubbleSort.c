6c050872677d4d5d8356247729a64a7d4d9891f6
evenrise01 <63799338+evenrise01@users.noreply.github.com>
2020-09-02 13:12:32 +0530

    Create BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,49 @@
+#include <stdio.h> 
+
+void swap(int *xp, int *yp) 
+{ 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
+} 
+
+
+void bubbleSort(int arr[], int n) 
+{ 
+int i, j; 
+for (i = 0; i < n-1; i++)	 
+
+
+	for (j = 0; j < n-i-1; j++) 
+		if (arr[j] > arr[j+1]) 
+			swap(&arr[j], &arr[j+1]); 
+} 
+
+
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i=0; i < size; i++) 
+		printf("%d ", arr[i]); 
+		printf("\n"); 
+} 
+
+ 
+int main() 
+{ 
+	int arr[1000]; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+    
+printf("Enter number of elements: \n");
+scanf("%d",&n);
+
+for (int i = 0; i< n; i++){
+
+    scanf("%d", &arr[i]);
+}
+    printArray(arr,n);
+	bubbleSort(arr, n); 
+	printf("Sorted array: \n"); 
+	printArray(arr, n); 
+	return 0; 
+} 
