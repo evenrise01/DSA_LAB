05dbfd3813aba875b46ba74aec4e7e7cd40ada70
evenrise01 <63799338+evenrise01@users.noreply.github.com>
2020-09-03 12:35:02 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,49 +1,48 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-void swap(int *xp, int *yp) 
-{ 
-	int temp = *xp; 
-	*xp = *yp; 
-	*yp = temp; 
-} 
+void BubbleSort(int arr[],int n){
+int temp;
+	for(int i =0; i<n-1;i++){
+		for (int j = 0; j<n-i-1;j++){
+			
+			if(arr[j] > arr[j+1])
 
+			temp = arr[j];    //temp variable = first variable
+			arr[j] = arr[j+1]; 
+			arr[j+1] = temp;
+		}
+	}
 
-void bubbleSort(int arr[], int n) 
-{ 
-int i, j; 
-for (i = 0; i < n-1; i++)	 
-
-
-	for (j = 0; j < n-i-1; j++) 
-		if (arr[j] > arr[j+1]) 
-			swap(&arr[j], &arr[j+1]); 
-} 
-
-
-void printArray(int arr[], int size) 
-{ 
-	int i; 
-	for (i=0; i < size; i++) 
-		printf("%d ", arr[i]); 
-		printf("\n"); 
-} 
-
- 
-int main() 
-{ 
-	int arr[1000]; 
-	int n = sizeof(arr)/sizeof(arr[0]); 
-    
-printf("Enter number of elements: \n");
-scanf("%d",&n);
-
-for (int i = 0; i< n; i++){
-
-    scanf("%d", &arr[i]);
 }
-    printArray(arr,n);
-	bubbleSort(arr, n); 
-	printf("Sorted array: \n"); 
-	printArray(arr, n); 
-	return 0; 
-} 
+
+void PrintArray(int arr[],int n){
+	int i;
+	for (i=0;i<n;i++){
+		printf("%d",arr[i]);
+		
+	}
+	printf("\n");
+}
+
+int main(){
+
+int arr[100];
+int n = sizeof(arr)/sizeof(arr[0]);
+
+printf("Enter the number of elements: \n");
+scanf("%d", &n);
+
+for (int i = 0; i< n; i++){  // Input of the Array
+	scanf("%d",&arr[i]);
+
+}
+
+PrintArray(arr,n); //Unsorted Array Print
+BubbleSort(arr,n);
+
+printf("Sorted Array: \n");
+PrintArray(arr,n); //Sorted Array Print
+
+return 0;
+
+}
